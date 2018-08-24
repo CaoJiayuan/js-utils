f83b075fc4f06fc07e451bd23cb31e8dbd77b001
cjy <cjy632258@hotmail.com>
2018-08-24 17:53:31 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import * as uploader from './lib/uploader';
 import * as functions from './lib/functions';
 import Signer from './lib/signer';
+import Storage from './lib/storage';
 
 export {
   uploader,
   functions,
   Signer,
+  Storage,
 }
