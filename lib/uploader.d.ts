19678efe1deaa0bcd9a12053c75a0f6f340afda1
cjy <cjy632258@hotmail.com>
2018-08-30 17:19:21 +0800

    types

--- lib/uploader.d.ts
+++ lib/uploader.d.ts
@@ -1,30 +1,33 @@
 declare interface UploadOptions {
-  progress ?: (process) => void,
-  validate ?: (uploadFile: UploadFile) => boolean,
-  chunk ?: boolean,
-  success ?: (data: any) => void,
-  error ?: (data: any) => void,
-  chunkSize ?: number,
-  driver ?: string,
-  name ?: string,
-  stsUrl ?: string
+    progress ?: (process) => void,
+    validate ?: (uploadFile: UploadFile) => boolean,
+    chunk ?: boolean,
+    success ?: (data: any) => void,
+    error ?: (data: any) => void,
+    chunkSize ?: number,
+    driver ?: string,
+    name ?: string,
+    stsUrl ?: string
 }
 
 declare interface UploadFile {
-  isImage: () => boolean,
-  isVideo: () => boolean,
-  getSize: () => number,
-  getExtension: () => string,
-  notExceeding: (size: number) => boolean,
-  invalidFileMessage: string,
+    isImage: () => boolean,
+    isVideo: () => boolean,
+    getSize: () => number,
+    getExtension: () => string,
+    notExceeding: (size: number) => boolean,
+    invalidFileMessage: string,
 }
 
 
-declare const fileUpload : (el: HTMLElement, url: string, configs ?: UploadOptions) => void;
-declare const upload : (file : File, url: string, configs ?: UploadOptions) => void;
-
+declare const fileUpload: (el: HTMLElement, url: string, configs ?: UploadOptions) => void;
+declare const upload: (file: File, url: string, configs ?: UploadOptions) => void;
+const DRIVER_OSS = 'oss';
+const DRIVER_SERVER = 'server';
 export {
-  fileUpload,
-  upload,
-  UploadFile
+    fileUpload,
+    upload,
+    UploadFile,
+    DRIVER_SERVER,
+    DRIVER_OSS
 }
